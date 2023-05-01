565d7463d0bec7be3779953252dbbbcd80f3a5c8
TheDarkCitoHD <benjamin.munoz.perez.inf@gmail.com>
2023-05-01 00:42:26 -0400

    Iniciar sesion listo js

---
+++ proyecto_semestral/js/inis.js
@@ -0,0 +1,50 @@
+const gmail = document.getElementById("v_email");
+const contrasenia = document.getElementById("v_contrasenia");
+const terminosYcondiciones = document.getElementById("termsAndConditions");
+const form = document.getElementById("form");
+const listInputs = document.querySelectorAll(".form-input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let condicion = validacionForm();
+  if (condicion) {
+    enviarFormulario();
+  }
+});
+
+function validacionForm() {
+  form.lastElementChild.innerHTML = " ";
+  let condicion = true;
+  listInputs.forEach((element) => {
+    element.lastElementChild.innerHTML = "";
+  });
+
+  if (gmail.value.length < 1 || gmail.value.trim() == "") {
+    mostrarMensajeError("v_email", "Ingrese un correo valido");
+    condicion = false;
+  }
+
+  if (contrasenia.value.length < 1 || contrasenia.value.trim() == "") {
+    mostrarMensajeError("v_contrasenia", "Ingrese una contraseña valida");
+    condicion = false;
+  } 
+  
+  if (!terminosYcondiciones.checked) {
+  mostrarMensajeError("termsAndConditions", "Acepte los terminos y condiciones");
+  condicion = false;
+  } else {
+  mostrarMensajeError("termsAndConditions", "");
+  }
+
+  return condicion;
+}
+
+function mostrarMensajeError(claseInput, mensaje) {
+  let elemento = document.querySelector(`.${claseInput}`);
+  elemento.lastElementChild.innerHTML = mensaje;
+}
+
+function enviarFormulario() {
+  form.reset();
+  form.lastElementChild.innerHTML = "Inicio Correctamente";
+}
